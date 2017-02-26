29ecf102244b4aef388e58e12bffd375a6b42cde
--global <--global>
2017-02-26 16:51:04 -0500

    Added requirements and class methods

---
+++ lib/scraper.rb
@@ -0,0 +1,15 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+class Scraper
+   
+    def self.scrape_search_results_page(search_page_url)
+        
+    end
+    
+    def self.scrape_craigslist_posting(posting_page_url)
+        
+    end
+    
+end
