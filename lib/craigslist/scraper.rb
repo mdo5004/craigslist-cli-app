3683253b1dd5eef49462581546788562ef9212de
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-02-26 17:07:42 -0500

    Import a cl search page with nokogiri

--- lib/craigslist/scraper.rb
+++ lib/craigslist/scraper.rb
@@ -5,7 +5,12 @@ require 'pry'
 class Craigslist::Scraper
    
     def self.scrape_search_results_page(search_page_url)
+        results_page = Nokogiri::HTML(open(search_page_url))
+        results = results_page.css("li.result-row")
         
+        results.each { |result| 
+            
+            }
     end
     
     def self.scrape_craigslist_posting(posting_page_url)
