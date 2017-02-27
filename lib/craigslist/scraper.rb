d67336cc145a54788108b2180a8e40ab87bc7be2
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-02-26 19:47:14 -0500

    #scrape_search_results_page scrapes title, price, neighborhood

--- lib/craigslist/scraper.rb
+++ lib/craigslist/scraper.rb
@@ -3,18 +3,26 @@ require 'nokogiri'
 require 'pry'
 
 class Craigslist::Scraper
-   
+
     def self.scrape_search_results_page(search_page_url)
         results_page = Nokogiri::HTML(open(search_page_url))
         results = results_page.css("li.result-row")
-        
+
+        results_hashes = Array.new
         results.each { |result| 
-            
+            title = result.css("a.result-title").text
+            price = result.css("span.result-price").text.scan(/(\$\d+)/).first
+            if price.class == Array
+                price = price[0]
+            end
+            neighborhood = result.css("span.result-hood").text
+            results_hashes << {title: title, price: price, neighborhood: neighborhood}
             }
+        results_hashes
     end
-    
+
     def self.scrape_craigslist_posting(posting_page_url)
-        
+
     end
-    
+
 end
