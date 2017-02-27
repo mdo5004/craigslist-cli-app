ba8e98dbb45571a3f76664f5d9649c3fc0b55be9
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-02-27 14:20:43 -0500

    Started basic CLI

---
+++ bin/craigslist.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require_relative "../config/environment.rb"
+
+puts "Thanks for using the Craigslist CLI"
+puts ""
+#puts "What is your zip code?"
+
+puts "What city are you in?"
+city = gets.strip.downcase
+
+
