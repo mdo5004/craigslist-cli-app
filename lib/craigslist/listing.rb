3baf47f9ad2ecb1da7f06813325ffe71c6915331
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-02-27 14:33:31 -0500

    Added class variable @@all + accessor, and #save

--- lib/craigslist/listing.rb
+++ lib/craigslist/listing.rb
@@ -2,9 +2,25 @@ class Craigslist::Listing
 
     attr_accessor :title, :price, :neighborhood, :latitude, :longitude, :description, :url
     
-    def initialize(data)
+    @@all = []
+    
+    def initialize(data = {})
         data.each { |k,v| 
             send("#{k}=",v)
             }
+    end
+    
+    def self.all
+        @@all
+    end
+    def save
+        self.class.all << self
+    end
+    
+    def self.find_by_title(title)
+        
+    end
+    
+    def self.find_or_create_by_title(title)
     end
 end
