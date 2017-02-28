ca948825d033a61ec311a7326129e34dbfb786f4
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-02-28 10:04:00 -0500

    Asks the user if they want to search, change location, or exit

--- bin/craigslist.rb
+++ bin/craigslist.rb
@@ -5,8 +5,36 @@ require_relative "../config/environment.rb"
 puts "Thanks for using the Craigslist CLI"
 puts ""
 
+running = true
+
 cls = Craigslist::Scraper.new
 
-puts "Let's get searching!"
-puts ""
-cls.search
+while running
+    puts "Let's get searching!"
+    puts ""
+    puts "Enter s to Search"
+    puts "Enter cl to Change Location"
+    puts "Enter e to Exit"
+    begin
+        user_input = STDIN.gets.strip.downcase
+    rescue
+        user_input = gets.strip.downcase
+    end
+
+    case user_input
+    when "s"
+        cls.search
+
+    when "cl"
+        cls.establish_location()
+
+    when "e"
+        running = false
+        break
+
+    else
+        puts "Invalid entry..."
+        puts ""
+
+    end
+end
